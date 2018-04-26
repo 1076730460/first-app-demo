edb86ef275383a94a9791b83175729ce8154da11
960075207@qq.com <273273wang>
2018-04-26 12:01:57 +0800

    提交jekins测试项目

--- src/main/java/com/example/firstappdemo/FirstAppDemoApplication.java
+++ src/main/java/com/example/firstappdemo/FirstAppDemoApplication.java
@@ -2,11 +2,18 @@ package com.example.firstappdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class FirstAppDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstAppDemoApplication.class, args);
 		System.out.println("Hello World");
+	}
+	@RequestMapping(value = "test")
+	public String Json(){
+		return "Hello World";
 	}
 }
